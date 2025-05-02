2cfdf06c75c5e31eead0d0177acacbe288a60759
Suman Shrestha <100428179+sumanhope@users.noreply.github.com>
2025-05-02 17:29:42 +1000

    Update HeroSection.tsx

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -39,7 +39,7 @@ export default function HeroSection() {
           <div className="glow absolute top-[40%] right-1/2 -z-10"></div>
         </div>
         <div className="absolute bottom-5 sm:bottom-14 left-0 sm:-left-10">
-          <a download href="./CV_Suman.pdf">
+          <a download href="./SumanShrestha_FlutterDeveloper_Resume.pdf">
             <MovingBorderBtn
               borderRadius="0.5rem"
               className="p-3 font-semibold "
